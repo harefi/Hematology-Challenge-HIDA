622cad216950703cadabcedad931b009e2b4ff42
Hadi H. Arefi <hadi.arephi@gmail.com>
2025-06-01 23:52:23 +0200

    chore: pre-commit autoformat

--- src/hematol/models.py
+++ src/hematol/models.py
@@ -1,9 +1,9 @@
 from torchvision import models
 import torch.nn as nn
 
+
 def build_baseline_model(num_classes: int):
     """ResNet-18 pretrained on ImageNet, FC layer resized."""
     model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
     model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
-
